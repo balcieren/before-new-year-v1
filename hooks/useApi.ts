887a392e39cadd12ea2ecbc2252e1e7ae08f8d8d
Eren BALCI <erenbalci3939@gmail.com>
2020-12-22 20:34:57 +0300

    Update useApi.ts

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -3,11 +3,11 @@ import Axios from "axios";
 export default function useAPI() {
   return Axios.create({
     baseURL: `${process.env.BASE_URL}:${process.env.PORT}/api`,
-    withCredentials: false,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    timeout: 10000,
+    // withCredentials: false,
+    // headers: {
+    //   Accept: "application/json",
+    //   "Content-Type": "application/json",
+    // },
+    // timeout: 10000,
   });
 }
