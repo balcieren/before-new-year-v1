296f717bfb598e2627b4e8b9b2f0018289701070
Eren BALCI <erenbalci3939@gmail.com>
2020-12-22 20:48:24 +0300

    Update useApi.ts

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -2,7 +2,8 @@ import Axios from "axios";
 
 export default function useAPI() {
   return Axios.create({
-    baseURL: `${process.env.BASE_URL}:${process.env.PORT}/api`,
+    baseURL: `${process.env.BASE_URL}/api`,
+    // baseURL: `${process.env.BASE_URL}:${process.env.PORT}/api`,
     // withCredentials: false,
     // headers: {
     //   Accept: "application/json",
