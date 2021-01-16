4de94007d0f31e60043e88cbcc9f2e5638f46abd
Eren BALCI <erenbalci3939@gmail.com>
2021-01-16 15:29:04 +0300

    added base_url

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -2,6 +2,6 @@ import Axios from "axios";
 
 export default function useAPI() {
   return Axios.create({
-    baseURL: `http://localhost:${process.env.PORT || 3000}/api`,
+    baseURL: `${process.env.BASE_URL || "http://localhost:3000"}/api`,
   });
 }
